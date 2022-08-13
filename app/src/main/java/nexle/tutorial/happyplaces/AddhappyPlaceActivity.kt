36bd5aaa7cfff5f0753cf3e92657f6f380317422
Phuc <phuclh@nexlesoft.com>
2022-08-13 19:37:44 +0700

    182. Setting up the Happy Place App

---
+++ app/src/main/java/nexle/tutorial/happyplaces/AddhappyPlaceActivity.kt
@@ -0,0 +1,11 @@
+package nexle.tutorial.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddhappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addhappy_place)
+    }
+}
